91c6528418b5a2f7232aadb2fa2e445c2e99bc33
Daniele Lacamera <dan@danielinux.net>
2020-05-15 00:20:29 +0200

    Added networking and BLE scan/adv

--- apps/pinetime/modules.mk
+++ apps/pinetime/modules.mk
@@ -5,10 +5,28 @@ USEMODULE += fonts
 #USEMODULE += bleman
 USEMODULE += event_timeout
 USEMODULE += hal
-USEMODULE += storage
+#USEMODULE += storage
 USEMODULE += util
 
 USEMODULE += shell
 USEMODULE += shell_commands
 USEMODULE += ps
 USEMODULE += schedstatistics
+
+# crypto
+USEPKG += wolfssl
+USEMODULE+=wolfcrypt wolfcrypt_aes_ctr wolfcrypt_aes
+
+# network
+USEMODULE += gnrc_netdev_default
+USEMODULE += auto_init_gnrc_netif
+USEMODULE += gnrc_ipv6_default
+USEMODULE += gnrc_sock_udp
+USEMODULE += periph_flashpage
+
+# BLE
+USEPKG += nimble
+USEMODULE += nimble_scanner
+USEMODULE += nimble_scanlist
+USEMODULE += nimble_svc_gap
+USEMODULE += nimble_svc_gatt
